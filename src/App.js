b3223e2bc1bd625dbfc908059e97b1f1fb17ba2c
JulioMh <kilojulio@gmail.com>
2020-01-09 18:44:34 +0100

    Limpiado algunas carpetas e instalado React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-   <p>Hello</p>
-  );
+    <Button variant="success">Success</Button>
+    );
 };
 
 export default App;
