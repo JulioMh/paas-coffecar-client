1d7f8e89da6c2b5c64983cd87916aa86db0e3f3d
JesusMarinGarcia <jesus1998mg@uma.es>
2020-01-09 23:30:27 +0100

    creado navbar en todas las "paginas", falta el log out

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './containers/Home/Home';
 import Trip from './containers/TripRoot/TripRoot';
 import Error from './components/Error/Error';
-//import Navigation from './components/Navigation/NavigationItems/NavigationItems'
+import Navigation from './components/Navigation/NavigationItems/NavigationItems'
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        
+        <header>
+          <Navigation />
+        </header>
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/trip/:id" Trip={Trip} />
+          <Route path="/trip/:id" component={Trip} />
           <Route path="/create-trip" component={Trip} />
           <Route component={Error} />
         </Switch>
---
+++ src/containers/TripRoot/TripRoot.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Trip extends Component {
+    render() {
+        return (
+                <h1>Trip</h1>
+            );
+    }
+}
+
+export default Trip;
