8d294d433215f0a32ce8d035e80cb0937901a884
JulioMh <kilojulio@gmail.com>
2020-01-10 19:30:54 +0100

    Es capaz de hacer peticiones POST que funcionas y he mejorado el diseño

--- src/containers/TripRoot/TripRoot.js
+++ src/containers/TripRoot/TripRoot.js
@@ -3,17 +3,20 @@ import Form from 'react-bootstrap/Form';
 import Label from 'react-bootstrap/FormLabel';
 import Input from 'react-bootstrap/FormControl';
 import FormGroup from 'react-bootstrap/FormGroup';
+import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 
 class TripRoot extends React.Component {
     state = {
-        writable: true,
+        writable: false,
         id: 0,
         title: '',
         departureTime: '',
-        arrivalTime: '',
+        arrivalDate: '',
         arrivalLatitude: '',
         arrivalLongitude: '',
         departureLatitude: '',
@@ -21,151 +24,222 @@ class TripRoot extends React.Component {
         description: '',
         imgLink: '',
         seats: '',
-        driver: '5e1768e1494e0738fe566ac7',
-        passengers: ''
+        driver: {
+            "id": "5e1768e1494e0738fe566ac7",
+            "name": "abcd",
+            "email": "abcd",
+            "ownedAnnounces": null,
+            "joinedAnnounces": null
+        },
+        passengers: null
     }
 
-onChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-}
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
 
-submitFormAdd = e => {
-    e.preventDefault()
-    fetch('https://coffeecar.herokuapp.com/api/announces', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: this.state.title,
-        seats: this.state.seats,
-        departureTime: this.state.departureTime,
-        arrivalTime: this.state.arrivalTime,
-        arrivalLatitude: this.state.arrivalLatitude,
-        arrivalLongitude: this.state.arrivalLongitude,
-        departureLatitude: this.state.departureLatitude,
-        departureLongitude: this.state.departureLongitude,
-        description: this.state.description,
-        imgLink: this.state.imgLink,
-        driver: this.state.driver,
-        passengers: this.state.passengers
-      })
-    })
-      .then(response => response.json())
-      .then(item => {
-        if(Array.isArray(item)) {
-          console.log('success')
-        } else {
-          console.log('failure')
+    submitFormEdit = e => {
+        e.preventDefault()
+        fetch('http://localhost:8080/api/announces', {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: this.state.title,
+                seats: this.state.seats,
+                departureTime: this.state.departureTime,
+                arrivalDate: this.state.arrivalDate,
+                arrivalLatitude: this.state.arrivalLatitude,
+                arrivalLongitude: this.state.arrivalLongitude,
+                departureLatitude: this.state.departureLatitude,
+                departureLongitude: this.state.departureLongitude,
+                description: this.state.description,
+                imgLink: this.state.imgLink,
+                driver: this.state.driver,
+                passengers: this.state.passengers
+            })
+        })
+            .then(response => response.json())
+            .then(item => {
+                if (Array.isArray(item)) {
+                    // console.log(item[0])
+                    this.props.updateState(item[0])
+                    this.props.toggle()
+                } else {
+                    console.log('failure')
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+
+    submitFormAdd = e => {
+        console.log(this.state);
+
+        e.preventDefault()
+        fetch('http://localhost:8080/api/announces', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: this.state.title,
+                seats: this.state.seats,
+                departureTime: this.state.departureTime,
+                arrivalDate: this.state.arrivalDate,
+                arrivalLatitude: this.state.arrivalLatitude,
+                arrivalLongitude: this.state.arrivalLongitude,
+                departureLatitude: this.state.departureLatitude,
+                departureLongitude: this.state.departureLongitude,
+                description: this.state.description,
+                imgLink: this.state.imgLink,
+                driver: this.state.driver,
+                passengers: this.state.passengers
+            })
+        })
+            .then(response => response.json())
+            .then(item => {
+                if (Array.isArray(item)) {
+                    // console.log(item[0])
+                    this.props.updateState(item[0])
+                    this.props.toggle()
+                } else {
+                    console.log('failure')
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+        if (this.props.item) {
+            const {
+                id,
+                title,
+                departureTime,
+                arrivalDate,
+                arrivalLatitude,
+                arrivalLongitude,
+                departureLatitude,
+                departureLongitude,
+                description,
+                imgLink,
+                seats,
+                driver,
+                passengers
+            } = this.props.item
+            this.setState({
+                id,
+                title,
+                departureTime,
+                arrivalDate,
+                arrivalLatitude,
+                arrivalLongitude,
+                departureLatitude,
+                departureLongitude,
+                description,
+                imgLink,
+                seats,
+                driver,
+                passengers
+            })
         }
-      })
-      .catch(err => console.log(err))
-  }
-
-componentDidMount(){
-    if (this.props.item) {
-        const { 
-            id, 
-            title, 
-            departureTime, 
-            arrivalTime, 
-            arrivalLatitude, 
-            arrivalLongitude, 
-            departureLatitude, 
-            departureLongitude, 
-            description, 
-            imgLink, 
-            seats, 
-            driver, 
-            passengers 
-        } = this.props.item
-        this.setState({             
-            id, 
-            title, 
-            departureTime, 
-            arrivalTime, 
-            arrivalLatitude, 
-            arrivalLongitude, 
-            departureLatitude, 
-            departureLongitude, 
-            description, 
-            imgLink, 
-            seats, 
-            driver, 
-            passengers})
     }
-}
 
-render() {
-    return (
-        <Form onSubmit={this.props.item ? this.submitFormEdit : this.submitFormAdd}>
-            <FormGroup>
-                <Label>Titulo</Label>
-                <Input
-                    type="text"
-                    name="title"
-                    id="title"
-                    plaintext={this.props.writable}
-                    readOnly={this.props.writable}
-                    onChange={this.onChange}
-                    value={this.state.title === null ? '' : this.state.title} />
-            </FormGroup>
+    render() {
+        return (
 
-            <FormGroup>
-                <Label>Asientos</Label>
-                <Input
-                    type="range"
-                    min="1"
-                    max="14"
-                    name="seats"
-                    id="seats"
-                    plaintext={this.props.writable}
-                    readOnly={this.props.writable}
-                    onChange={this.onChange}
-                    value={this.state.seats === null ? '' : this.state.seats} />
-            </FormGroup>
+            <Card style={{ width: '40%', margin: '70px', marginTop: '70px', boxShadow: "5px 5px 5px grey" }}>
+                <Card.Body>
+                    <Card.Title>{this.props.item ? 'Información del viaje' : '¡Publica tu viaje ahora!'}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Que no se te olviden las llaves :P</Card.Subtitle>
+                    <Form onSubmit={this.props.item ? this.submitFormEdit : this.submitFormAdd}>
+                        <FormGroup>
+                            <Label>Titulo</Label>
+                            <Input
+                                type="text"
+                                name="title"
+                                id="title"
+                                plaintext={this.state.writable}
+                                readOnly={this.state.writable}
+                                onChange={this.onChange}
+                                value={this.state.title === null ? '' : this.state.title} />
+                        </FormGroup>
+                        <Label>Asientos</Label>
+                        <Form.Row>
+                            <FormGroup as={Col}>
+                                <Input
+                                    type="range"
+                                    min="1"
+                                    max="14"
+                                    name="seats"
+                                    id="seats"
+                                    plaintext={this.state.writable}
+                                    readOnly={this.state.writable}
+                                    onChange={this.onChange}
+                                    value={this.state.seats === null ? '' : this.state.seats} />
+                            </FormGroup>
+                            <FormGroup as={Col} md="auto">
+                                <Input
+                                    style={{ width: "50px" }}
+                                    type="text"
+                                    name="seats"
+                                    id="seats"
+                                    plaintext={this.state.writable}
+                                    readOnly={this.state.writable}
+                                    onChange={this.onChange}
+                                    value={this.state.seats === null ? '' : this.state.seats} />
+                            </FormGroup>
+                        </Form.Row>
 
-            <Form.Row>
-                <FormGroup as={Col}>
-                    <Label>Fecha de salida</Label>
-                    <Input
-                        type="datetime-local"
-                        name="departureTime"
-                        id="departureTime"
-                        plaintext={this.props.writable}
-                        readOnly={this.props.writable}
-                        onChange={this.onChange}
-                        value={this.state.departureTime === null ? '' : this.state.departureTime} />
-                </FormGroup>
-                <FormGroup as={Col}>
-                    <Label>Fecha de llegada</Label>
-                    <Input
-                        type="datetime-local"
-                        name="arrivalTime"
-                        id="arrivalTime"
-                        plaintext={this.props.writable}
-                        readOnly={this.props.writable}
-                        onChange={this.onChange}
-                        value={this.state.arrivalTime === null ? '' : this.state.arrivalTime} />
-                </FormGroup>
-            </Form.Row>
+                        <Form.Row>
+                            <FormGroup as={Col}>
+                                <Label>Fecha de salida</Label>
+                                <Input
+                                    type="datetime-local"
+                                    name="departureTime"
+                                    id="departureTime"
+                                    plaintext={this.state.writable}
+                                    readOnly={this.state.writable}
+                                    onChange={this.onChange}
+                                    value={this.state.departureTime === null ? '' : this.state.departureTime} />
+                            </FormGroup>
+                            <FormGroup as={Col}>
+                                <Label>Fecha de llegada</Label>
+                                <Input
+                                    type="datetime-local"
+                                    name="arrivalDate"
+                                    id="arrivalDate"
+                                    plaintext={this.state.writable}
+                                    readOnly={this.state.writable}
+                                    onChange={this.onChange}
+                                    value={this.state.arrivalDate === null ? '' : this.state.arrivalDate} />
+                            </FormGroup>
+                        </Form.Row>
 
-            <FormGroup>
-                <Label>Descripción</Label>
-                <Input
-                    as="textarea"
-                    name="description"
-                    id="description"
-                    row="3"
-                    plaintext={this.props.writable}
-                    readOnly={this.props.writable}
-                    onChange={this.onChange}
-                    value={this.state.description === null ? '': this.state.description} />
-            </FormGroup>
-            <Button>Submit</Button>
-        </Form>
-    );
-}
+                        <FormGroup>
+                            <Label>Descripción</Label>
+                            <Input
+                                as="textarea"
+                                name="description"
+                                id="description"
+                                row="3"
+                                plaintext={this.state.writable}
+                                readOnly={this.state.writable}
+                                onChange={this.onChange}
+                                value={this.state.description === null ? '' : this.state.description} />
+                        </FormGroup>
+                        <Row className="justify-content-md-center">
+                            <Col md="auto">
+                                <Button className="justify-content-md-center" style={{ marginLeft: "auto" }} type="submit" >Confirmar</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Card.Body>
+            </Card >
+
+        );
+    }
 }
 
 export default TripRoot;
