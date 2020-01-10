fcbc357bff86bb73cdcea1c0c2317dfbeaf0239d
JesusMarinGarcia <jesus1998mg@uma.es>
2020-01-10 20:56:01 +0100

    commit para poder mergear la rama axios

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './containers/Home/Home';
 import Trip from './containers/TripRoot/TripRoot';
 import Error from './components/Error/Error';
+import Wellcome from './containers/Wellcome/Wellcome'
 import Navigation from './components/Navigation/NavigationItems/NavigationItems';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 import Map from './Map'
-import {GMap} from 'primereact/gmap';
+import { GMap } from 'primereact/gmap';
 
 
 
 class App extends Component {
   render() {
     const options = {
-      center: {lat: 36.890257, lng: 30.707417},
+      center: { lat: 36.890257, lng: 30.707417 },
       zoom: 12
-  };
+    };
     return (
       <BrowserRouter>
         <div>
@@ -26,9 +27,10 @@ class App extends Component {
             <Navigation />
           </header>
           <Map></Map>
-          <GMap options={options} style={{width: '100%', minHeight: '320px'}} />
+          <GMap options={options} style={{ width: '100%', minHeight: '320px' }} />
           <Switch>
-            <Route path="/" component={Home} exact />
+            <Route path="/" component={Wellcome} exact />
+            <Route path="/home" component={Home} exact />
             <Route path="/trip/:id" component={Trip} />
             <Route path="/create-trip" component={Trip} />
             <Route component={Error} />
