67d002314e734763296c32d7fee0f111f3c3d0a8
JavierC07 <36677101+JavierC07@users.noreply.github.com>
2020-01-10 18:41:05 +0100

    mek

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import CoffeeCarLogo from '../../Logo/CoffeeCarLogo.png'
+//import { NavLink } from 'react-router-dom';
+
+const NavigationItems = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">
+                <img
+                    alt=""
+                    src={CoffeeCarLogo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+            </Navbar.Brand>
+            <Navbar.Brand href="/">CoffeeCar</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="#link">Log out</Nav.Link>                    
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default NavigationItems;
