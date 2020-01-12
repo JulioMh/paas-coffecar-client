2ae4054d78672f36502e843ae9f86163d79e793a
JavierC07 <36677101+JavierC07@users.noreply.github.com>
2020-01-12 12:38:54 +0100

    cambio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home/Home';
+import Trip from './containers/TripRoot/TripRoot';
+import Error from './components/Error/Error';
+import Navigation from './components/Navigation/NavigationItems/NavigationItems';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 function App() {
   return (
-   <p>Hello</p>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Navigation />
+        </header>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/trip/:id" component={Trip} />
+          <Route path="/create-trip" component={Trip} />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
