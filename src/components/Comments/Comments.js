5bd4b5c16cbac103354c66e4d97bd8acebb9af78
JesusMarinGarcia <jesus1998mg@uma.es>
2020-01-20 13:41:27 +0100

    seguimos trabajando

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,24 +4,32 @@ import Reply from './Reply/Reply'
 import Comment from './Comment/Comment'
 
 
-const comments = (props) => {
-    return (
-        <Card style={{ width: '75%', margin: 'auto', marginTop: '20px', boxShadow: "5px 5px 5px grey" }}>
-            <Card.Body>
-            <Reply id={props.id} isReply={false} />
-            {props.comments.map(co => (
-                <Comment 
-                    key = {co.id}
-                    id = {co.id}
-                    user = {co.user}
-                    date = {co.date}
-                    text = {co.text}
-                />
+class Comments extends React.Component {
 
-            ))}
-            </Card.Body>
-        </Card>
-    )
+    state = {
+        comments: null
+    }
+
+
+    render() {
+        return (
+            <Card style={{ width: '75%', margin: 'auto', marginTop: '20px', boxShadow: "5px 5px 5px grey" }}>
+                <Card.Body>
+                    <Reply id={props.id} isReply={false} />
+                    {props.comments.map(co => (
+                        <Comment
+                            key={co.id}
+                            id={co.id}
+                            user={co.user}
+                            date={co.date}
+                            text={co.text}
+                        />
+
+                    ))}
+                </Card.Body>
+            </Card>
+        )
+    }
 }
 
 export default comments;
--- src/components/Comments/Reply/Reply.js
+++ src/components/Comments/Reply/Reply.js
@@ -9,7 +9,7 @@ class Reply extends Component {
     };
 
     handleSubmit() {
-
+        
     }
 
     render() {
